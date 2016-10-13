3fb710fb572b06aaa9ec19cee70d89006f6560f8
Jeff Rector <rector_jeffrey@hotmail.com>
2016-10-13 17:34:04 -0400

    Content Parser working for base cases

---
+++ ContentParser.Tests/ParserTests.cs
@@ -0,0 +1,123 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Dynamic;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Newtonsoft.Json;
+
+namespace ContentParser.Tests
+{
+    [TestClass]
+    public class ParserTests
+    {
+        private string styleString;
+        private string dataString;
+
+        [TestInitialize]
+        public void Initialize()
+        {
+           
+        }
+
+        [TestMethod]
+        public void TestStyleMapping()
+        {
+            Generate5eStyle();
+            Generate5eData();
+
+            var result = ContentParser.ConvertToHtml(dataString, styleString);
+
+            Assert.IsNotNull(result);
+
+        }
+
+
+        private void Generate5eStyle()
+        {
+            dynamic style = new ExpandoObject();
+
+            style.sectionorder = new[] { "header", "divider", "general", "divider", "statblock", "divider", "abilities", "divider", "tactics", "actiondivider", "actions" };
+
+            style.sections = new Dictionary<string, ExpandoObject>();
+                  
+            style.sections["divider"] = new ExpandoObject();
+            style.sections["divider"].type = "divider";
+                  
+            style.sections["header"] = new ExpandoObject();
+            style.sections["header"].type = "items";
+            style.sections["header"].itemorder = new[] { "name", "type" };
+            style.sections["header"].items = new Dictionary<string, string>();
+            style.sections["header"].items["name"] = "## [name]";
+            style.sections["header"].items["type"] = "*[type]*";
+                  
+            style.sections["general"] = new ExpandoObject();
+            style.sections["general"].type = "items";
+            style.sections["general"].itemorder = new[] {"ac","hp","spd"};
+            style.sections["general"].items = new Dictionary<string, string>();
+            style.sections["general"].items["ac"] = "**Armor Class:** [ac]";
+            style.sections["general"].items["hp"] = "**Hit Points:** [hp] ([hd])";
+            style.sections["general"].items["spd"] = "**Speed:** [spd]";
+                  
+            style.sections["statblock"] = new ExpandoObject();
+            style.sections["statblock"].type = "table";
+            style.sections["statblock"].headers = new[] { "STR", "DEX", "CON", "INT", "WIS", "CHA" };
+            style.sections["statblock"].row = "|[str]|[dex]|[con]|[intel]|[wis]|[cha]|";
+                  
+            style.sections["abilities"] = new ExpandoObject();
+            style.sections["abilities"].type = "list";
+            style.sections["abilities"].format = "**[key]:** [value]";
+                 
+            style.sections["tactics"] = new ExpandoObject();
+            style.sections["tactics"].type = "list";
+            style.sections["tactics"].format = "**[key]:** [value]";
+                 
+            style.sections["actiondivider"] = new ExpandoObject();
+            style.sections["actiondivider"].type = "header";
+            style.sections["actiondivider"].itemorder = new [] {"title", "divider"};
+            style.sections["actiondivider"].items = new Dictionary<string, string>();
+            style.sections["actiondivider"].items["title"] = "### Actions";
+            style.sections["actiondivider"].items["divider"] = "___";
+                 
+            style.sections["actions"] = new ExpandoObject();
+            style.sections["actions"].type = "list";
+            style.sections["actions"].format = "**[key]:** [value]";
+
+            styleString = JsonConvert.SerializeObject(style);
+        }
+
+        private void Generate5eData()
+        {
+            dynamic data = new ExpandoObject();
+
+            data.name = "Ghost Pigeon";
+            data.type = "Small fiend, chaotic sloppy";
+            data.ac = "18";
+            data.hp = "25";
+            data.hd = "3d8+2";
+            data.spd = "30 ft.";
+            data.str = "12";
+            data.dex = "13";
+            data.con = "12";
+            data.intel = "13";
+            data.wis = "12";
+            data.cha = "13";
+            data.abilities = new Dictionary<string, string>
+            {
+                {"Condition Immunities", "melancholy, groggy"},
+                {"Senses","passive Perception"},
+                {"Languages","Common"},
+                {"Challenge","12 (5173 XP)"}
+            };
+            data.tactics = new Dictionary<string, string>
+            {
+                {"Pack Tactics","These guys work together. Like super well, you don't even know." }
+            };
+            data.actions = new Dictionary<string, string>
+            {
+                {"Crossface Suplex.","*Melee Weapon Attack:* +4 to hit, reach 5ft., one target. *Hit* 5 (1d6 + 2) "},
+                {"Jumping Driver.","*Melee Weapon Attack:* +4 to hit, reach 5ft., one target. *Hit* 5 (1d6 + 2) "},
+            };
+
+            dataString = JsonConvert.SerializeObject(data);
+        }
+    }
+}
---
+++ ContentParser/ContentParser.cs
@@ -0,0 +1,92 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Dynamic;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using MarkdownDeep;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
+
+namespace ContentParser
+{
+    internal class SectionTypes
+    {
+        internal static string Divider = "divider";
+        internal static string Items = "items";
+        internal static string Table = "table";
+        internal static string List = "list";
+        internal static string Header = "header";
+    }
+
+
+    public class ContentParser
+    {
+        private static readonly Regex propertyRegex = new Regex(@"\[(\w+)\]");
+
+        public static string ConvertToHtml(string data, string style)
+        {
+
+            dynamic styleObject;
+            dynamic dataObject;
+
+            var md = new Markdown();
+            var output = new StringBuilder();
+
+            try
+            {
+                styleObject = JsonConvert.DeserializeObject(style);
+                dataObject = JsonConvert.DeserializeObject<ExpandoObject>(data);
+            }
+            catch (JsonSerializationException ex)
+            {
+                return $"<p>Error deserializing a parameter.  ex:{ex.Message}</p>";
+            }
+
+            foreach (string section in styleObject.sectionorder)
+            {
+                if (styleObject.sections[section].type == SectionTypes.Divider)
+                {
+                    output.Append("<hr/>");
+                }
+                else if (styleObject.sections[section].type == SectionTypes.Header)
+                {
+                    foreach (string item in styleObject.sections[section].itemorder)
+                    {
+                        output.Append(md.Transform((string) styleObject.sections[section].items[item]));
+                    }
+                }
+                else if (styleObject.sections[section].type == SectionTypes.List)
+                {
+                    foreach (
+                        KeyValuePair<string, object> listItem in
+                            (IDictionary<string, object>) ((IDictionary<string, object>) dataObject)[section])
+                    {
+                        var format = (string) styleObject.sections[section].format;
+
+                        output.Append(md.Transform(format.Replace("[key]", listItem.Key)
+                            .Replace("[value]", listItem.Value.ToString())));
+                    }
+                }
+                else if (styleObject.sections[section].type == SectionTypes.Items)
+                {
+                    foreach (string item in styleObject.sections[section].itemorder)
+                    {
+                        var itemString = (string) styleObject.sections[section].items[item];
+
+                        foreach (Match match in propertyRegex.Matches(itemString))
+                        {
+                            itemString = itemString.Replace(match.Groups[0].Value,
+                                (string) ((IDictionary<string, object>) dataObject)[match.Groups[1].Value]);
+                        }
+
+                        output.Append(md.Transform(itemString));
+                    }
+                }
+            }
+
+            return output.ToString();
+        }
+
+    }
+}
